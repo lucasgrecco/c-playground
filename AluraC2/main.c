15ea8f3231fc0d169c934bbd2c0e794e4b6d4336
Lucas Grecco <lucasgrecco@gmail.com>
2018-08-27 15:37:35 -0300

    C2 Start

--- AluraC2/main.c
+++ AluraC2/main.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <jmorecfg.h>
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+  char palavra_secreta[20];
+  char chute;
+  bool acertou = false;
+  bool enforcou = false;
+
+
+  sprintf(palavra_secreta, "manjola");
+
+  printf("Jogo da ROLA\n");
+  printf("%s", palavra_secreta);
+
+
+
+  do {
+    printf("Chute: ");
+    scanf(" %c", &chute);
+    for (int i = 0; i < strlen(palavra_secreta); i++) {
+      if (palavra_secreta[i] == chute){
+        printf("A posição %d tem essa letra.\n", i+1);
+      }
+    }
+
+  } while (!acertou && !enforcou);
+  return 0;
 }
